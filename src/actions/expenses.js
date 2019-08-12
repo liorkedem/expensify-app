108d8e39b0afc64e5049b1c762dddb899a1ee873
Lior Kedem <lior.kdm@gmail.com>
2019-08-12 14:54:30 +0300

    store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -19,7 +19,8 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-    return (dispatch) => {
+    return (dispatch,getState) => {
+        const uid = getState().auth.uid;
         const {
             description = '',
             note = '',
@@ -29,7 +30,7 @@ export const startAddExpense = (expenseData = {}) => {
 
         const expense = { description, note, amount, createdAt };
 
-        return database.ref('expenses').push(expense)
+        return database.ref(`users/${uid}/expenses`).push(expense)
             .then((ref) => {
                 dispatch(addExpense({
                     id: ref.key,
@@ -50,8 +51,9 @@ export const removeExpense = (
 });
 
 export const startRemoveExpense = ({ id } = {}) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).remove()
+    return (dispatch,getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses/${id}`).remove()
             .then(() => {
                 dispatch(removeExpense({ id }));
             });
@@ -65,8 +67,9 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-    return (dispatch) => {
-        return database.ref(`expenses/${id}`).update(updates)
+    return (dispatch,getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses/${id}`).update(updates)
             .then(() => {
                 dispatch(editExpense(id, updates));
             });
@@ -79,8 +82,9 @@ export const setExpenses = (expenses) => ({
 });
 
 export const startSetExpenses = () => {
-    return (dispatch) => {
-        return database.ref('expenses').once('value')
+    return (dispatch,getState) => {
+        const uid = getState().auth.uid;
+        return database.ref(`users/${uid}/expenses`).once('value')
             .then((snapshot) => {
                 const expenses = [];
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = ({ startLogout }) => (
     <div>
         <header>
             <h1>Expensify</h1>
-            <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+            <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
             <NavLink to="/create" activeClassName="is-active">Create</NavLink>
             <button onClick={startLogout}>Logout</button>
         </header>
